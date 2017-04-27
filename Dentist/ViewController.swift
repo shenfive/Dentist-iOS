032f71d8d0e7302d952dc7a30611e10ad5e6b0c0
shenfive @ Mac <shenfive@gmail.com>
2017-04-28 03:41:34 +0800

    com mode add and commit

--- Dentist/ViewController.swift
+++ Dentist/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button01: UIButton!
+    @IBOutlet weak var button02: UIButton!
+    @IBOutlet weak var button03: UIButton!
+    @IBOutlet weak var button04: UIButton!
+    @IBOutlet weak var newReservationBut: UIButton!
+    @IBOutlet weak var myReservationBut: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.title = NSLocalizedString("title", comment: "")
+        button01.setTitle(NSLocalizedString("tab1", comment: ""), for: .normal)
+        button02.setTitle(NSLocalizedString("tab2", comment: ""), for: .normal)
+        button03.setTitle(NSLocalizedString("tab3", comment: ""), for: .normal)
+        button04.setTitle(NSLocalizedString("tab4", comment: ""), for: .normal)
+    newReservationBut.setTitle(NSLocalizedString("newReservation", comment: ""), for: .normal)
+        myReservationBut.setTitle(NSLocalizedString("myReservation", comment: ""), for: .normal)
+        
     }
 
     override func didReceiveMemoryWarning() {
